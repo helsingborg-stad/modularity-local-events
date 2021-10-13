2b143a9885ec7383d7f3a557b7bea70a0d3938b8
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-10-13 10:37:43 +0200

    Update according to spec
    - Remove show more button (automatic determination replacement).
    - Fix issue where expired events was shown on archive page
    - Restructure loading of views

--- source/php/Module/LocalEvents.php
+++ source/php/Module/LocalEvents.php
@@ -27,14 +27,13 @@ class LocalEvents extends \Modularity\Module
     public function data() : array
     {
         $data = array();
-        $fieldNamespace = 'mod_localevents_';
-        $fields = get_fields();  
+        $fields = get_fields();
 
-        //Map module data to camel case vars
-        $data['events'] = $this->getPosts($fields['number_of_events']);
-        $data['events'] = $this->formatEvents($data['events']);
-        $data['archiveLink'] = get_post_type_archive_link('local-events');
-        $data['enableMoreEventsButton'] = $fields['enable_more_events_button'];
+        $data['archiveLink']            = get_post_type_archive_link('local-events');
+
+        $eventStack                     = $this->getPosts($fields['number_of_events'] ?? 5);
+        $data['events']                 = $this->formatEvents($eventStack['posts']);
+        $data['totalEvents']            = $eventStack['postcount']; 
 
         //Translations
         $data['lang'] = (object) array(
@@ -45,40 +44,64 @@ class LocalEvents extends \Modularity\Module
         return $data;
     }
 
-    private function getPosts($numberOfEvents) {
-        $today = date('Ymd');
-        $args = [
+    /**
+     * Get the posts to show, include max number of posts that can be fetched
+     *
+     * @param integer $numberOfEvents   The maximum number of items to fetch
+     * @return array                    Posts array including maimum number of matching items
+     */
+    private function getPosts($numberOfEvents = 5) {
+
+        $query = new \WP_Query(array(
             'post_type' => 'local-events',
-            'numberposts' => $numberOfEvents,
+            'posts_per_page' => $numberOfEvents,
             'post_status' => 'publish',
-            'meta_query' => array(array( 'key' => 'date', 'value' => $today, 'compare' => '>=' )),
+            'meta_query' => array(array( 'key' => 'date', 'value' => date('Ymd'), 'compare' => '>=' )),
             'meta_key' => 'date',
             'orderby' => 'meta_value_num',
             'order' => 'ASC',
             'suppress_filters' => true
-        ];
+        ));
 
-        return get_posts($args);
+        if(!is_wp_error($query)) {
+            return [
+                'postcount' => $query->found_posts,
+                'posts' => $query->posts
+            ];
+        }
+
+        return false; 
     }
 
+    /**
+     * Format event array
+     *
+     * @param array $events
+     * @return array
+     */
     public function formatEvents($events) {
 
-        foreach ($events as $key => $event) {
-            $fields     = get_fields($event->ID);
-            $timestamp  = strtotime($fields['date']);
-            $year       = date("Y", $timestamp);
+        if(is_array($events) && !empty($events)) {
 
-            $event->day         = date("j", $timestamp);
-            $event->monthShort  = __(date("M", $timestamp), 'local-events');
-            $event->month       = __(date("F", $timestamp), 'local-events');
+            foreach ($events as $key => $event) {
 
-            $event->dateFormatted = "{$event->day} {$event->month} {$year}, {$fields['start_time']}";
-
-            if($fields['end_time']) {
-                $event->dateFormatted = $event->dateFormatted . "- {$fields['end_time']}";
+                $fields     = get_fields($event->ID);
+                $timestamp  = strtotime($fields['date']);
+                $year       = date("Y", $timestamp);
+    
+                $event->day         = date("j", $timestamp);
+                $event->monthShort  = __(date("M", $timestamp), 'local-events');
+                $event->month       = __(date("F", $timestamp), 'local-events');
+                $event->link        = get_permalink($event->ID);
+    
+                $event->dateFormatted = "{$event->day} {$event->month} {$year}, {$fields['start_time']}";
+    
+                if($fields['end_time']) {
+                    $event->dateFormatted = $event->dateFormatted . "- {$fields['end_time']}";
+                }
+    
+                $events[$key] = $event;
             }
-
-            $events[$key] = $event;
         }
 
         return $events;
