1f41ca80bacae1a5c8edc68be7a00a6c46069d7b
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-12-10 11:21:22 +0100

    Use more of the default single template
    Added more slots to allow for this in municpio

--- views/single-local-events.blade.php
+++ views/single-local-events.blade.php
@@ -1,63 +1,30 @@
 @extends('templates.single')
 
-@section('layout')
-<div class="o-container">
-    <div class="o-grid u-margin__top--4">
-        @if(!empty($event['image_src']))
-            <div class="o-grid-12 o-grid-8@lg">
-                @image([
-                    'src'=> $event['image_src'],
-                ])
-                @endimage
-            </div>
-        @endif
-        
-        <div class="o-grid-12 o-grid-8@lg modularity-local-event-heading">
-            <div class="modularity-local-event-date-box">
-                @typography(['variant' => 'h1', 'element' => 'span'])
-                    {{ $event['day'] }}
-                @endtypography
-
-                @typography(['variant' => 'h4', 'element' => 'span'])
-                    {{ $event['monthShort'] }}
-                @endtypography
-            </div>
-
-            @typography([
-                'variant' => 'h1',
-                'element' => 'h1',
-                'classList' => ['modularity-local-event-title']
-            ])
-                {{ the_title() }}
+@section('article-after-featured-image')   
+    <div class="localevent-meta c-paper">
+        <div class="localevent-meta__date">
+            @typography(['variant' => 'h1', 'element' => 'span'])
+                {{ $event['day'] }}
             @endtypography
 
+            @typography(['variant' => 'h4', 'element' => 'span'])
+                {{ $event['monthShort'] }}
+            @endtypography
         </div>
-                
-        <div class="o-grid-12 o-grid-8@lg">
+
+        <div class="localevent-meta__metadata">
+
             @if (!empty($event['dateFormatted']))
                 @typography(['variant' => 'h4', 'element' => 'h4'])
                     {{ $event['dateFormatted'] }}
                 @endtypography
             @endif
-
+            
             @icon(['icon' => 'map']) @endicon
+
             @typography(['element' => 'span'])
                 {{$event['place']}}
             @endtypography
-
-        </div>
-
-        <div class="o-grid-12 o-grid-8@lg">
-            <article class="c-article">
-                @if (!empty(get_extended($post->postContentFiltered)['main']) && !empty(get_extended($post->postContentFiltered)['extended']))
-                    {!! get_extended($post->postContentFiltered)['main'] !!}
-                    {!! get_extended($post->postContentFiltered)['extended'] !!}
-                @else
-                    {!! $post->postContentFiltered !!}
-                @endif
-            </article>
         </div>
     </div>
-</div>
 @stop
-
