7a314134c5375e1f7f2fc5007e967c18afae0071
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-08-31 13:27:28 +0200

    Autoload & Build

--- modularity-local-events.php
+++ modularity-local-events.php
@@ -27,14 +27,11 @@ define('MODULARITYLOCALEVENTS_MODULE_PATH', MODULARITYLOCALEVENTS_PATH . 'source
 
 load_plugin_textdomain('modularity-local-events', false, plugin_basename(dirname(__FILE__)) . '/languages');
 
-require_once MODULARITYLOCALEVENTS_PATH . 'source/php/Vendor/Psr4ClassLoader.php';
+// Autoload from plugin
+if (file_exists(MODULARITYLOCALEVENTS_PATH . 'vendor/autoload.php')) {
+    require_once MODULARITYLOCALEVENTS_PATH . 'vendor/autoload.php';
+}
 require_once MODULARITYLOCALEVENTS_PATH . 'Public.php';
-
-// Instantiate and register the autoloader
-$loader = new ModularityLocalEvents\Vendor\Psr4ClassLoader();
-$loader->addPrefix('ModularityLocalEvents', MODULARITYLOCALEVENTS_PATH);
-$loader->addPrefix('ModularityLocalEvents', MODULARITYLOCALEVENTS_PATH . 'source/php/');
-$loader->register();
 
 // Acf auto import and export
 $acfExportManager = new \AcfExportManager\AcfExportManager();
@@ -45,42 +42,12 @@ $acfExportManager->autoExport(array(
     'local-events' => 'group_6153072da2c66'
 ));
 $acfExportManager->import();
-/*
-// Modularity 3.0 ready - ViewPath for Component library
-add_filter('/Modularity/externalViewPath', function ($arr) {
-    $arr['mod-local-events'] = MODULARITYLOCALEVENTS_MODULE_VIEW_PATH;
-    return $arr;
-}, 10, 3);*/ 
-
-// Add view paths
-add_filter('Municipio/blade/view_paths', function ($array){
-    // If child theme is active, insert plugin view path after child views path.
-    /*if (is_child_theme()) {
-        array_splice( $array, 2, 0, array(EVENTMANAGERINTEGRATION_VIEW_PATH) );
-    } else {
-        // Add view path first in the list if child theme is not active.
-        array_unshift($array, EVENTMANAGERINTEGRATION_VIEW_PATH);
-    }*/ 
-
-    return $array;
-}, 2, 1);
 
 // Modularity 3.0 ready - ViewPath for Component library
 add_filter('/Modularity/externalViewPath', function ($arr) {
     $arr['mod-local-events'] = MODULARITYLOCALEVENTS_MODULE_VIEW_PATH;
     return $arr;
 }, 10, 3);
-
-/**
- * Add searchable blade template paths
- * @param array  $array Template paths
- * @return array        Modified template paths
- */
-
-
-
-
-
 
 // Start application
 new ModularityLocalEvents\App();
